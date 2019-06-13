6c0250a6f65cc23d27c299c62bd2faffc9ed253b
tarek <tarek.demachkie@gmail.com>
2019-06-13 18:49:04 -0500

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 module.exports = {
     mode: 'production',
+    // mode: 'development',
     entry: {
       index: './src/index.js',
     //   print: './src/print.js'
